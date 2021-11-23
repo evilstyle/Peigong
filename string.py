e83682a9102991edc0a78f9a1c90bbb06dbfbeff
Dazhuang <daemonstyle5@gmail.com>
2021-11-23 17:26:27 +0800

    循环和格式化字符学习

---
+++ string.py
@@ -0,0 +1,10 @@
+name = "Dog"
+age = 2
+text = " %s今年%d岁"%(name,age)
+print(text)
+text1="ws{}，sdf{}".format ("dog","12")
+print(text1)
+name ="dog"
+age =23
+text2 = f"oh {name},sdf{age}"
+print(text2)
